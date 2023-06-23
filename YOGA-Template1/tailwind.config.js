65996c31b6dc02a0de234bfb8a9d81b123300612
Abhishek Soni <83975832+ABHI8120@users.noreply.github.com>
2023-06-24 00:45:00 +0530

    YOGA-Template1
    Create a yoga template

---
+++ YOGA-Template1/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      color:{
+        'bg-rgba':'rgba(255, 82, 181, 1)',
+      }
+    },
+  },
+  plugins: [],
+}
+
